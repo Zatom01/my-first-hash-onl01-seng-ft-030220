fa5d38e69fe02daf3fea91e595a816458c03fd30
Anil Gurung <anil_gurung15@yahoo.com>
2020-03-07 04:28:23 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,5 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+  your_hash=Hash.new
   your_hash=["name"=>"Anil","height"=>"6ft"]
 
   
